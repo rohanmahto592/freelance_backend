64ff31b086e3d6b3e26d22be2eadb9ed914c961b
rohan <rohan.mahto@shl.com>
2023-08-16 19:01:05 +0530

    minro fix for deliverysheet

--- Utils/UpdateExcelSheetOrderHelper.js
+++ Utils/UpdateExcelSheetOrderHelper.js
@@ -1,39 +1,54 @@
 const XLSX = require("xlsx");
 const { UpdateOrderModelHelper } = require("./UpdateOrderModelHelper");
 
-function UpdateExcelSheet(workbook,excelSheetRef){
+function UpdateExcelSheet(workbook, excelSheetRef) {
+  let workbook_sheetNames = workbook.SheetNames;
+  return new Promise((resolve, reject) => {
+    const promises = workbook_sheetNames.map((sheetName) => {
+      const worksheet = workbook.Sheets[sheetName];
+      let AWB_NO_Data;
 
-    let workbook_sheetNames = workbook.SheetNames;
-    return new Promise( (resolve,reject)=>{
-        
-        const promises= workbook_sheetNames.map(sheetName => {
-            const worksheet = workbook.Sheets[sheetName];
-            let AWB_NO_Data;
-          
-            switch (sheetName) {
-              case 'Dispatched':
-                AWB_NO_Data = XLSX.utils.sheet_to_json(worksheet)?.map(item =>({ 'trackingId': item['AWB NO'], 'application id': item['application id'],jsonData:item,excelSheetRef }));
-                break;
-          
-              case 'ShipRocket_Delivery':
-                AWB_NO_Data = XLSX.utils.sheet_to_json(worksheet)?.map(item => ({ 'trackingId': item['AWB NO'], 'application id': item['application id'],jsonData:item,excelSheetRef }));
-                break;
-          
-              case 'IndianPost_Delivery':
-                AWB_NO_Data = XLSX.utils.sheet_to_json(worksheet)?.map(item => ({ 'trackingId': item['AWB NO'], 'application id': item['application id'],jsonData:item,excelSheetRef }));
-                break;
-            }
-            if (AWB_NO_Data?.length>0) {
-               return (UpdateOrderModelHelper(AWB_NO_Data));
-                  
-              
-              }
-            
-    })
-    Promise.all(promises).then((response)=>{
-        resolve(response);
+      switch (sheetName) {
+        case "Dispatched":
+          AWB_NO_Data = XLSX.utils.sheet_to_json(worksheet)?.map((item) => ({
+            trackingId: item["AWB NO"],
+            "application id": item["application id"],
+            jsonData: item,
+            excelSheetRef: excelSheetRef,
+          }));
+          break;
+
+        case "ShipRocket_Delivery":
+          AWB_NO_Data = XLSX.utils.sheet_to_json(worksheet)?.map((item) => ({
+            trackingId: item["AWB NO"],
+            "application id": item["application id"],
+            jsonData: item,
+            excelSheetRef: excelSheetRef,
+          }));
+          break;
+
+        case "IndianPost_Delivery":
+          AWB_NO_Data = XLSX.utils.sheet_to_json(worksheet)?.map((item) => ({
+            trackingId: item["AWB NO"],
+            "application id": item["application id"],
+            jsonData: item,
+            excelSheetRef: excelSheetRef,
+          }));
+          break;
+      }
+      if (AWB_NO_Data?.length > 0) {
+        return UpdateOrderModelHelper(AWB_NO_Data);
+      }
     });
-})
+
+    Promise.all(promises)
+      .then((response) => {
+        resolve(response.filter((response) => response !== undefined));
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
-module.exports={UpdateExcelSheet}
+module.exports = { UpdateExcelSheet };
--- controllers/deliveryController.js
+++ controllers/deliveryController.js
@@ -1,50 +1,36 @@
-
 const { getUserDeliveryExcelRefById } = require("../Models/orderModel");
 const xlsx = require("xlsx");
 const { UpdateExcelSheet } = require("../Utils/UpdateExcelSheetOrderHelper");
-const uploadDeliveryExcelSheet=(req,res)=>{
-    const excelSheetBufferData=req.file.buffer;
-    const userId=req.body.userDeliveryId;
-    console.log(userId)
-    getUserDeliveryExcelRefById(userId).then((getExcelSheetRef)=>{
-      if(!getExcelSheetRef.success)
-      {
-          res.send({success:false,message:"No excelSheet is found for this deliverySheet"});
-      }
-      else
-      {
-          const workbook = xlsx.read(excelSheetBufferData, { type: "buffer" });
-       UpdateExcelSheet(workbook,getExcelSheetRef.message).then((response)=>{
-        response=response.filter((response)=>response!=undefined);
-        if(response)
-        {
-          res.send(response[0]);
-        }
-        else
-        {
-          res.send({success:false,message:'internal server error,try again after sometime'})
-        }
-       });
-      
-       
-        
-      
-         
-          // let workbook_response = xlsx.utils.sheet_to_json(
-          //   workbook.Sheets[workbook_sheets[0]]
-          // );
-          // console.log(typeof workbook_response);
-          // workbook_response.forEach(row => {
-            
-          //   });
-      }
-  
-
-    });
-   
-
-
-
-
-}
-module.exports={uploadDeliveryExcelSheet};
+const uploadDeliveryExcelSheet = (req, res) => {
+  const excelSheetBufferData = req.file.buffer;
+  const userId = req.body.userDeliveryId;
+  console.log(userId);
+  getUserDeliveryExcelRefById(userId).then((getExcelSheetRef) => {
+    if (!getExcelSheetRef.success) {
+      res.send({
+        success: false,
+        message: "No excelSheet is found for this deliverySheet",
+      });
+    } else {
+      const workbook = xlsx.read(excelSheetBufferData, { type: "buffer" });
+      UpdateExcelSheet(workbook, getExcelSheetRef.message)
+        .then((response) => {
+          if (response.length > 0) {
+            res.send(response[0]);
+          } else {
+            res.send({
+              success: false,
+              message: "Internal server error, try again after some time.",
+            });
+          }
+        })
+        .catch((error) => {
+          res.send({
+            success: false,
+            message: "Internal server error, try again after some time.",
+          });
+        });
+    }
+  });
+};
+module.exports = { uploadDeliveryExcelSheet };
